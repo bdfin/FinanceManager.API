5ddf47e53d2944dc79019c7314d3720e5eb105a8
Beau <beau.d.findlay@gmail.com>
2021-02-07 00:47:29 +0000

    Add startup with azure app config.

---
+++ FinanceManager.API/FinanceManager.API/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+
+[assembly: FunctionsStartup(typeof(FinanceManager.API.Startup))]
+
+namespace FinanceManager.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("FinManagerConfigConnectionString");
+
+            builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(connectionString)
+                    .ConfigureKeyVault(kv =>
+                    {
+                        kv.SetCredential(new DefaultAzureCredential());
+                    });
+            });
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
